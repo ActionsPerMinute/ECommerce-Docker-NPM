cd40f5891d8bbac117111a14d8a9f874376df68a
shivloka <shiv.loka@appdynamics.com>
2015-07-24 11:13:07 -0700

    changed env.sh to accept web host and port and removed lbr

--- ECommerce-Load/env.sh
+++ ECommerce-Load/env.sh
@@ -17,16 +17,24 @@ fi
 
 # 4th param to LoadRunner call
 if [ -z "${TARGET_HOST}" ]; then
-	export TARGET_HOST="lbr";
+	export TARGET_HOST="web";
 fi
 
 # 5th param to LoadRunner call
-if [ -z "${TARGET_PORT}" ]; then
-        export TARGET_PORT="80";
+if [ -z "${TARGET_ANGULARHOST}" ]; then
+	export TARGET_ANGULARHOST="angular";
 fi
 
 # 6th param to LoadRunner call
+if [ -z "${TARGET_PORT}" ]; then
+        export TARGET_PORT="8080";
+fi
+
+# 7th param to LoadRunner call
+if [ -z "${TARGET_ANGULARPORT}" ]; then
+        export TARGET_ANGULARPORT="8080";
+fi
+# 8th param to LoadRunner call
 if [ -z "${WAIT_TIME}" ]; then
         export WAIT_TIME="1000";
 fi
-
